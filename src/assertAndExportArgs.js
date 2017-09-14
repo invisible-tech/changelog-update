e64770c0095014a272632be658996c637f67fd5f
rodrigo4244 <rodrigo4244@users.noreply.github.com>
2017-09-14 08:24:58 -0300

    feat: assertAndExportArgs.js

---
+++ src/assertAndExportArgs.js
@@ -0,0 +1,53 @@
+'use strict'
+
+/* eslint no-console: 0 */
+
+const path = require('path')
+const {
+  compact,
+  flow,
+  head,
+  includes,
+  split,
+  toLower,
+} = require('lodash/fp')
+
+const pathConsumerPackage = path.join(process.cwd(), 'package.json')
+const { 'release-note': releaseNote } = require(pathConsumerPackage)
+const { EMOJI_LIST } = require('./constants')
+
+const {
+  'slackbot-name': slackbotName,
+  'icon-emoji': iconEmoji,
+} = releaseNote
+
+const logWarningAndExit = arg => {
+  console.log(`WARNING: ${arg} argument is missing.`)
+  process.exit(1)
+}
+if (! slackbotName) logWarningAndExit('slackbot name')
+if (! iconEmoji) logWarningAndExit('icon emoji')
+
+const logAndExit = arg => {
+  console.log(`WARNING: set ${arg} on your project package.json!`)
+  process.exit(1)
+}
+if (slackbotName === '<your-slackbot-name>') logAndExit('<your-slackbot-name>')
+if (iconEmoji === '<your-slackbot-emoji OR slack-emoji>') logAndExit('<your-slackbot-emoji OR slack-emoji>')
+
+const iconEmojiLower = toLower(iconEmoji)
+const splitAndReplaceColons = str => flow(
+  split(':'),
+  compact,
+  head
+)(str)
+const newIconEmoji = iconEmoji.includes(':') ? splitAndReplaceColons(iconEmojiLower) : iconEmojiLower
+
+if (! includes(newIconEmoji)(EMOJI_LIST)) console.log('release-note: you are using a custom emoji')
+
+const _iconEmoji = `:${newIconEmoji}:`
+
+module.exports = {
+  slackbotName,
+  iconEmoji: _iconEmoji,
+}
