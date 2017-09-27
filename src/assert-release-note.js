79ee60a7c2d867d46a5e4de4e0ebd57d1beb752f
rodrigo4244 <rodrigo4244@users.noreply.github.com>
2017-09-27 17:44:25 -0300

    refact: skip when in production and staging branch too

--- src/assert-release-note.js
+++ src/assert-release-note.js
@@ -5,18 +5,17 @@
 /* eslint-disable no-console */
 
 const execa = require('execa')
-
+const { includes } = require('lodash/fp')
 require('./assertAndExportArgs.js')
 
 const { CIRCLE_BRANCH } = process.env
+const ignoredBranches = ['master', 'production', 'staging']
+const shouldIgnore = branch => includes(branch)(ignoredBranches)
 
-if (CIRCLE_BRANCH === 'master') {
-  console.log('This is master, skipping.')
-  process.exit(0)
-}
-
-const { stdout: testChangelog } = execa.sync('git', ['diff', '--name-only', 'origin/master...HEAD', '--', 'CHANGELOG.md'])
-if (! testChangelog) {
-  console.log('This PR is missing a Release Note in CHANGELOG.md')
-  process.exit(1)
-}
+if (! shouldIgnore(CIRCLE_BRANCH)) {
+  const { stdout: testChangelog } = execa.sync('git', ['diff', '--name-only', 'origin/master...HEAD', '--', 'CHANGELOG.md'])
+  if (! testChangelog) {
+    console.log('This PR is missing a Release Note in CHANGELOG.md')
+    process.exit(1)
+  }
+} else console.log(`This is ${CIRCLE_BRANCH}, skipping.`)
